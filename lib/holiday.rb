8e3967de6d4d54dbfceab88f020e8d551ecf3177
Tim Donoghue <tmd112593@gmail.com>
2020-02-24 00:51:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,6 +49,10 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  new_array = []
+  holiday_hash[:winter].each do |holiday, supply|
+    new_array << 
+   
 
 end
 
