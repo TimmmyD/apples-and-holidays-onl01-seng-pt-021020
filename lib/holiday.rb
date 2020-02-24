8f050e6c5c0c5bfd9c278893b05e9cec4863a567
Tim Donoghue <tmd112593@gmail.com>
2020-02-24 01:26:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays_hash|
     puts (season.to_s.capitalize + ":")
     holidays_hash.each do |holiday, supplies|
-       result << (holiday.to_s.capitalize + ": ")
+       result << (holiday.to_s.split('_').map(&:capitalize).join(' ') + ": ")
       supplies.each_with_index do |supply, index|
         if index == supplies.length - 1
           result << supply
