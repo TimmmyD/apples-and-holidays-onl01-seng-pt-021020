a2cba7f316efa464659dce3618523a4553cc7879
Tim Donoghue <tmd112593@gmail.com>
2020-02-24 01:09:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,6 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  result = ""
   holiday_hash.each do |season, holidays_hash|
     puts season + ":"
     holidays_hash.each do |holiday, supplies|
